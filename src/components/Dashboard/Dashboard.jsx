06d87f7f16ee60078334492864a68647daffb1ce
Hujaifa21 <21hujaifa@gmail.com>
2021-11-13 02:09:57 +0600

    semi final

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -7,7 +7,7 @@ const DashBoard = () => {
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/checkAdmin/${user?.email}`)
+    fetch(`https://shielded-brushlands-06342.herokuapp.com/checkAdmin/${user?.email}`)
       .then((res) => res.json())
       .then((data) => {
         if (data[0]?.role === "admin") {
